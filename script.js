739206d274b9c23fbbfb2c9639a145adaeb70912
valentin <valentinvacher01@gmail.com>
2022-08-07 20:25:04 +0200

    black screen

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const canvas = document.getElementById('bomberman');
+const play = document.getElementById('play');
+
 let ctx = canvas.getContext('2d');
+
+play.addEventListener('click', () => {
+    play.style.transition = '0s'
+    play.style.visibility = 'hidden';
+    canvas.style.background = '#000';
+})
