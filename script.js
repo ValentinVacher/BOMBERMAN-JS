b1e7eb18ece62229ce1b04e1df39fc5cf04b1a20
valentin <valentinvacher01@gmail.com>
2022-08-09 11:31:51 +0200

    remake all game

--- script.js
+++ script.js
@@ -1,97 +1,69 @@
 const canvas = document.getElementById('bomberman');
-const canvasWidth = canvas.width = 1920;
-const canvasHeight = canvas.height = 1080;
 const play = document.getElementById('play');
 const ctx = canvas.getContext('2d');
+
+canvas.width = 1920;
+canvas.height = 1080;
 
 play.addEventListener('click', () => {
     play.style.transition = '0s'
     play.style.visibility = 'hidden';
     canvas.style.background = '#eee';
 
-    launchGame();
+    window.addEventListener('load', () => {
+        class InputHandler {
+
+        }
+
+        class Particle {
+
+        }
+
+        class Player {
+            constructor(game, x, y) {
+                this.game = game;
+                this.width = 120;
+                this.height = 130;
+                this.x = x;
+                this.y = y;
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+
+            update() {
+                this.x += this.speedX;
+                this.y += this.speedY
+            }
+
+            draw(context){
+                context.fillRect(this.x, this.y, this.width, this.height)
+            }
+        }
+
+        class Background {
+
+        }
+
+        class UI {
+
+        }
+
+        class Game {
+            constructor(width, height) {
+                this.width = width;
+                this.height = height;
+                this.player = new Player(this, 0, 0);
+            }
+
+            update() {
+                this.Player.update();
+            }
+
+            draw(context) {
+                this.player.draw(context);
+            }
+        }
+
+        const game = new Game(canvas.width, canvas.height);
+    })
 });
-
-function launchGame() {
-    const linkWidth = 120;
-    const linkHeight = 130;
-    const staggerFrame = 5;
-
-    let link = new Image();
-    let gameFrame = 0;
-    let linkState = 'walkRight';
-
-    link.src = "./asset/animation/link.png";
-
-    const linkAnimation = [];
-    const linkStates = [
-        {
-            name : 'idleBot',
-            frames : 1
-        },
-        {
-            name : 'idleLeft',
-            frames : 1
-        },
-        {
-            name : 'idleTop',
-            frames : 1
-        },
-        {
-            name : 'idleRight',
-            frames : 1
-        },
-        {
-            name : 'walkBot',
-            frames : 10
-        },
-        {
-            name : 'walkLeft',
-            frames : 10
-        },
-        {
-            name : 'walkTop',
-            frames : 10
-        },
-        {
-            name : 'walkRight',
-            frames : 10
-        }
-    ];
-
-    linkStates.forEach((state, index) => {
-        let frames = {
-            loc : [],
-        };
-
-        for(let j = 0; j < state.frames; j++){
-            let positionX = j * linkWidth;
-            let positionY = index * linkHeight;
-
-            frames.loc.push(
-                {
-                    x : positionX, 
-                    y : positionY
-                });
-        }
-
-        linkAnimation[state.name] = frames;
-    });
-
-    console.log(linkAnimation);
-
-    function animate(){
-        ctx.clearRect(0, 0, 1920, 1080);
-    
-        let position = Math.floor(gameFrame/staggerFrame) % linkAnimation[linkState].loc.length;
-        let frameX = linkWidth * position;
-        let frameY = linkAnimation[linkState].loc[position].y;
-    
-        ctx.drawImage(link, frameX, frameY, linkWidth, linkHeight, 0, 0, linkWidth, linkHeight);
-    
-        gameFrame++;
-        requestAnimationFrame(animate);
-    }
-
-    animate();
-}
